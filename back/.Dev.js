6737cd56c99cef9a941296c0aab0e8e84804aeac
Abe Menashy <abestbet1@gmail.com>
2016-03-22 13:37:43 -0700

    modified .Dev. file

--- back/.Dev.js
+++ back/.Dev.js
@@ -1,3 +1,10 @@
 module.exports.DB = 'mongodb://Studio18:Studio18@ds047075.mlab.com:47075/kiwi'
 
 module.exports.PORT = 3000
+
+module.exports.gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: '',
+  publicKey: '',
+  privateKey: ''
+  })
