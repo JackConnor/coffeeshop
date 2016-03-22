795272b08f5e59086ac380375c9d9a9bf92123ea
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2016-03-22 14:19:47 -0700

    Adds sockets

--- back/routes/orderRoutes.js
+++ back/routes/orderRoutes.js
@@ -6,10 +6,12 @@ var orderCtrl = require( '../controllers/orderController.js' )
 auth.route('')
     .get( orderCtrl.index )
     .post( orderCtrl.create )
+	.put( orderCtrl.allUser )
 
 auth.route('/one')
     .post( orderCtrl.show )
     .put( orderCtrl.update )
+	.patch( orderCtrl.completed )
     .delete( orderCtrl.destroy )
 
 module.exports = auth
