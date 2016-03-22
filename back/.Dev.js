869041dd4519e9a1769339b70a51291e7caa7ac7
Abe Menashy <abestbet1@gmail.com>
2016-03-22 13:54:27 -0700

    added in routes in routes.js

--- back/.Dev.js
+++ back/.Dev.js
@@ -2,9 +2,9 @@ module.exports.DB = 'mongodb://Studio18:Studio18@ds047075.mlab.com:47075/kiwi'
 
 module.exports.PORT = 3000
 
-module.exports.gateway = braintree.connect({
-  environment: braintree.Environment.Sandbox,
-  merchantId: '',
-  publicKey: '',
-  privateKey: ''
-  })
+//braintree
+module.exports.merchantId = ""
+module.exports.publicKey = ""
+module.exports.privateKey = ""
+
+
--- back/routes/routes.js
+++ back/routes/routes.js
@@ -7,6 +7,7 @@ var userRoutes = require( './userRoutes.js' )
 var orderRoutes = require( './orderRoutes.js')
 var authCtrl   = require( '../controllers/authController.js' )
 var itemRoutes = require( './itemRoutes.js' )
+var paymentRoutes = require( './paymentRoutes.js' )
 
 Router.use( '/users', userRoutes.noAuth )
 Router.use( '/items', itemRoutes )
