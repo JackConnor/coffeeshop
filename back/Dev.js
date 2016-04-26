afbb50a74c893adfa5ca2ddf950cdb8920d728e4
Jack Connor <jack.connor83@gmail.com>
2016-04-26 10:11:18 -0700

    another attempt

--- back/Dev.js
+++
@@ -1,9 +0,0 @@
-module.exports.DB = 'mongodb://Studio18:Studio18@ds047075.mlab.com:47075/kiwi'
-
-module.exports.PORT = 3000
-
-
-//braintree
-module.exports.merchantId = 'ms4ywz6hq4r2hp45',
-module.exports.publicKey = '7vjmpg9dyczpss3z',
-module.exports.privateKey = '9f7f8095b0d4fa316fc3f48c031d9c35'
